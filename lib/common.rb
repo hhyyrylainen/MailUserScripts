cc0ce6175d6ad75daca7e9ed5e68414bb6caef41
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-11-20 20:36:24 +0200

    Refactored a bit and added check for minimum password length

--- lib/common.rb
+++ lib/common.rb
@@ -22,35 +22,47 @@ def create_db
   Database.new
 end
 
+def generate_random_password
+  @options[:password] = SecureRandom.alphanumeric(RANDOM_PASSWORD_LENGTH)
+  puts 'Using randomly generated password'
+  @random_password = true
+end
+
+def password_from_user_confirm
+  suppress_echo
+  print 'New password: '
+  password = STDIN.gets.chomp
+  puts ''
+  print 'Confirmation: '
+  password_confirm = STDIN.gets.chomp
+  puts ''
+  enable_echo
+
+  [password, password_confirm]
+end
+
+def prompt_stdin_password
+  password = nil
+
+  loop do
+    password, confirm = password_from_user_confirm
+
+    break if password == confirm
+
+    puts "Passwords don't match! Please retry."
+  end
+
+  @options[:password] = password
+
+  abort 'You did not enter a password when prompted' unless @options[:password]
+end
+
 def handle_password_option
-  unless @options[:password]
-    @options[:password] = SecureRandom.alphanumeric(RANDOM_PASSWORD_LENGTH)
-    puts 'Using randomly generated password'
-    @random_password = true
-  end
+  generate_random_password unless @options[:password]
 
-  if @options[:password] == '-'
-    password = nil
+  prompt_stdin_password if @options[:password] == '-'
 
-    loop do
-      suppress_echo
-      print 'New password: '
-      password = STDIN.gets.chomp
-      puts ''
-      print 'Confirmation: '
-      password_confirm = STDIN.gets.chomp
-      puts ''
-      enable_echo
-
-      break if password == password_confirm
-
-      puts "Passwords don't match! Please retry."
-    end
-
-    @options[:password] = password
-
-    abort 'You did not enter a password when prompted' unless @options[:password]
-  end
+  abort "Password can't be less than 12 characters" if @options[:password].size < 12
 end
 
 def print_password_if_needed
